c8893a96907c3cc487c4cfb95da54df322635039
Pilar890 <pilarvillarreal546@gmail.com>
2023-07-10 13:21:04 -0300

    cambios

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://rajshekhar26.github.io/cleanfolio',
-  title: 'JS.',
 }
 
 const about = {
@@ -9,11 +8,10 @@ const about = {
   name: 'Pilar Angelina Villarreal',
   role: 'Full Stack Developer',
   description:
-    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  resume: 'https://example.com',
+    "Hello! I'm Pilar, Full Stack web programmer, as a Full Stack web developer, I have a passion for technology and web development, and I am always looking for opportunities, always looking to learn to improve my skills and knowledge. I am a committed and organized person, which allows me to work efficiently in a team and meet deadlines.",
   social: {
-    linkedin: 'https://linkedin.com',
-    github: 'https://github.com',
+    linkedin: 'https://www.linkedin.com/in/pilar-villarreal/',
+    github: 'https://github.com/Pilar890',
   },
 }
 
@@ -21,26 +19,66 @@ const projects = [
   // projects can be added an removed
   // if there are no projects, Projects section won't show up
   {
-    name: 'Project 1',
+    name: 'ODYSSEY - ECOMMERCE',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+      'Aplicación web de comercio especializada en la venta y personalización de paquetes turísticos de alta calidad y experiencias inolvidables.',
+    stack: [
+      'HTML',
+      'CSS',
+      'React',
+      'Javascript',
+      'API REST',
+      'Nodejs',
+      'MongoDb',
+      'Redux',
+      'React Native',
+      'Express',
+      'Git',
+      'Github',
+    ],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Project 2',
+    name: 'Minga',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+      'Aplicación web de mangas, diseño app Web y Mobile para lectura, creación y edición de mangas',
+    stack: [
+      'HTML',
+      'CSS',
+      'React',
+      'Javascript',
+      'API REST',
+      'Nodejs',
+      'MongoDb',
+      'Express',
+      'Git',
+      'Github',
+    ],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
   },
   {
-    name: 'Project 3',
+    name: 'PetShop',
     description:
-      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
-    stack: ['SASS', 'TypeScript', 'React'],
+      'Aplicación web de comercio especializada en la venta de productos exclusivos para el cuidado y bienestar de mascotas.',
+    stack: ['HTML', 'CSS', 'Javascript', 'API REST'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Amazing Events',
+    description:
+      'Aplicación web de gestión y organización de una amplia variedad de eventos.',
+    stack: ['HTML', 'CSS', 'Javascript', 'API REST'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Sunflower',
+    description:
+      'Aplicación web especializada en plantas que ofrece una completa guía informativa sobre el cuidado y mantenimiento de jardines.',
+    stack: ['HTML', 'CSS'],
     sourceCode: 'https://github.com',
     livePreview: 'https://github.com',
   },
@@ -49,23 +87,26 @@ const projects = [
 const skills = [
   // skills can be added or removed
   // if there are no skills, Skills section won't show up
-  'HTML',
+  'HTML5',
   'CSS',
   'JavaScript',
-  'TypeScript',
+  'Trello',
   'React',
-  'Redux',
-  'SASS',
-  'Material UI',
+  'Redux.js',
+  'POO',
+  'Express.js',
   'Git',
-  'CI/CD',
-  'Jest',
-  'Enzyme',
+  'GitHub',
+  'Mongoose',
+  'MongoDB',
+  'Node.js',
+  'Bootstrap',
+  'React Native',
 ]
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'pilarvillarreal546@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
